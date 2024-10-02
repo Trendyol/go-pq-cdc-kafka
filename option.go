221e0b1216baf8bc734f38a42cdd796a04a7e809
Berna Akman <berna-akman@outlook.com>
2024-10-02 18:49:12 +0300

    feat: add option

---
+++ option.go
@@ -0,0 +1,34 @@
+package go_pq_cdc_kafka
+
+import (
+	"github.com/Trendyol/go-dcp-cdc-kafka/kafka"
+)
+
+type Option func(Connector)
+
+type Options []Option
+
+func (ops Options) Apply(c Connector) {
+	for _, op := range ops {
+		op(c)
+	}
+}
+
+func WithResponseHandler(responseHandler kafka.SinkResponseHandler) Option {
+	return func(c Connector) {
+		c.(*connector).responseHandler = responseHandler
+	}
+}
+
+/*func WithPrometheusMetrics(collectors []prometheus.Collector) Option {
+	return func(c Connector) {
+		c.(*connector).metrics = collectors
+	}
+}
+
+func WithLogger(l logger.Logger) Option {
+	return func(c Connector) {
+		c.(*connector).cfg.CDC.Logger.Logger = l
+	}
+}
+*/
